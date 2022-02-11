f9158bb2bd0b8b78ad060eb5f958681670d2b5b7
sofuetakuma <kawahagi0620@gmail.com>
2022-02-12 04:58:11 +0900

    socket.ioのcorsのoriginを戻した

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://suumo-mapping.netlify.app"],
+    origin: "*",
   },
 });
 
